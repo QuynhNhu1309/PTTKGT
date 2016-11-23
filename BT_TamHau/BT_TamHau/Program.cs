832b600ce15d35c80a39635d8e171ce964a11d60
QuynhNhu1309 <quynhnhu130995@gmail.com>
2016-11-23 09:21:03 +0700

    BT_TamHau_NguoiDiDuLich

---
+++ BT_TamHau/BT_TamHau/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_TamHau
+{
+    class Program
+    {
+        static int r = 0, n = 8, z = 1;
+        static int[] column = new int[9];
+        static string[,] pos = new String[9, 9];
+        static void HienThi()
+        {
+            Console.Write("\n\n\t\t---- GIAI THUAT MIN-MAX _ BAI TOAN TAM HAU -----\n\n\n\n");
+        }
+
+        static void Search()
+        {
+            r = n % 12;
+            if (r == 8)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                        if (i % 2 == 0)
+                        {
+                            column[z] = i;//col[1] = 2; col[2] = 4; col[3] = 6; col[4] = 8;
+                            z++;
+                        }
+
+                        if (i % 2 != 0)
+                        {
+                            column[z + 4] = i;//col[5] = 1; col[6] = 3; col[7] = 5; col[8] = 7;
+                        }
+                }
+
+                int j = 5;
+                while (j <= n && column[j] < column[j + 1])
+                {
+                    int tmp = column[j];
+                    column[j] = column[j + 1];
+                    column[j + 1] = tmp;//col[6] = 1; col[5] = 3; col[7] = 7; col[8] = 5;
+                    j = j + 2;
+                }
+               
+
+            }
+        }
+
+        static void Xuat()
+        {
+            Console.Write("\t");
+            for (int i = 1; i <= 8; i++)
+            {  
+                Console.Write("\t {0} ", i);   
+            }
+            Console.Write("\t\t\t____________________________________________________________");
+            Console.WriteLine("\n \t    |");
+            
+
+            for (int i = 1; i <= 8; i++)
+            {
+                Console.Write("\t {0}  |", i);
+                for (int j = 1; j <= n; j++)
+                {
+                    int f = column[i];
+                    //Console.Write(" f = {0} ", f);
+                    if ( j != f)//nếu  j(cột) trùng với f thì chọn vị trí này. Vì theo thuật toán hàng là stt, còn cột là danh sách 
+                        //mảng column
+                    {
+                        pos[i, f] = "_";
+                    }
+                    else
+                    {
+                        pos[i, f] = "X";
+                    }
+                    Console.Write(" \t {0} ", pos[i, f]);
+                }
+                Console.Write("\t\t    | \n ");
+                Console.WriteLine("\t    | ");
+                
+            }
+
+            Console.Write("\n\n Tam vi tri cua quan hau co the la: \n");
+            for (int i = 1; i <= 8; i++)
+            {
+                    int f = column[i];
+                    Console.Write(" \t ({0}, {1}) \n ", i, f);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            HienThi();
+            Search();
+            Xuat();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Geedy Search_SaleMan/Geedy Search_SaleMan/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geedy_Search_SaleMan
+{
+    class Program
+    {
+
+            static int n = 0;//số đỉnh
+            static int[] tour = new int[10];
+            static int s = 0;//đỉnh xuất phát
+            //static int min = 0;
+            static int length_value = 0;
+            static int[,] a = new int[10, 10];//độ dài cung của từ đỉnh này đến đỉnh khác
+            static int point = 0;
+            static int cost = 0;
+            static void Nhap()
+            {
+                Console.WriteLine("\n\n\t\t----- THUAT TOAN THAM LAM - BAI TOAN NGUOI BAN HANG ----");
+                Console.Write("\n\n\t\tNhap so dinh: ");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("\n\n\t\tNhap do dai tung cung: ");
+            
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = i + 1; j <= n; j++)
+                    {
+                        Console.Write("\n\t\ta[{0},{1}] = ", i, j);
+                        length_value = int.Parse(Console.ReadLine());
+                        a[i, j] = length_value;
+                        a[j, i] = a[i,j];
+                    }
+                    a[i, i] = 0;
+                }
+
+                do
+                {
+                    Console.Write("\n\n\t\tChon dinh xuat phat: ");
+                    s = int.Parse(Console.ReadLine());
+                    tour[1] = s;
+                }
+                while (s <= 0 || s > n);
+            }
+            static void Xuat()
+            {
+            
+                Console.WriteLine("\n\n\t\tMa tran: ");
+                
+                for (int i = 1; i <= n; i++)
+                {   Console.Write("\t\t\t");
+                    for (int j = 1; j <= n; j++)
+                    {
+                        Console.Write("{0}", a[i, j]);
+                        Console.Write("\t");
+                    }
+                    Console.WriteLine("\n");
+                }
+               
+            }
+
+            static bool KiemTra(int[] tour, int v, int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (tour[i] == v)//Kiểm tra đỉnh này đã đi qua chưa
+                        return false;
+                }
+                return true;
+            }
+
+            static int TopNear(int s)
+            {
+                int min = 1000;
+                
+                for (int j = 1; j <= n; j++)
+                {
+                    bool test = KiemTra(tour, j, n);
+                    if (a[s, j] > 0 && a[s, j] <= min && test == true)//Nếu chưa đi qua và là đỉnh có cung nhỏ nhất
+                    {
+                        min = a[s, j];
+                        point = j;
+                    }
+
+                    if (a[s, j] > 0 && tour[n] != 0 && j == tour[1])//điều kiện thỏa đỉnh cuối cùng
+                    {
+                        point = tour[1];
+                    }
+
+                }
+                return point;
+            }
+
+            static void Findway(int s)
+            {
+                int e = s;
+                Console.Write("\n\t\tPATH = {0} --> ", s);
+                for (int i = 2; i <= n +1 ; i++)
+                {
+                    int next = TopNear(e);//tìm đỉnh có độ dài cung nhỏ nhất
+                    cost = cost + a[e, next];
+                    tour[i] = next;
+                    e = next;
+                    
+                    if (i != n + 1)
+                    {
+                        Console.Write("{0} --> ", tour[i]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", tour[i]);
+                    }
+                }
+                Console.Write("\n\n\t\tChi phi = {0}", cost);
+            }
+
+
+            static void Main(string[] args)
+            {
+                Nhap();
+                Xuat();
+                Findway(s);
+                Console.ReadLine();
+            }
+    }
+}
